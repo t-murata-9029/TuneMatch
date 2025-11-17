847d23b5469a20e992c7453dd8d44141eebbbe5a
murata <t.murata.lol.9029@gmail.com>
2025-11-17 10:59:06 +0900

    music_reviewsを追加したよ

--- types/db.ts
+++ types/db.ts
@@ -20,12 +20,21 @@ export interface User {
   created_at?: Date | string | null; // timestamptz (多くの場合NOT NULLですが、オプショナルとしました)
 
   /* Spotify 関連トークン */
-   spotify_access_token?: string | null; // text
-   spotify_refresh_token?: string | null; // text
+  spotify_access_token?: string | null; // text
+  spotify_refresh_token?: string | null; // text
 }
 
-export interface Spotify_app_token{
+export interface Spotify_app_token {
   access_token: string;
   expires_in: number;
+  created_at: Date;
+}
+
+export interface Music_reviews {
+  id: number;
+  user_id: string;
+  track_id: string;
+  review_text: string;
+  rating: number;
   created_at: Date;
 }
