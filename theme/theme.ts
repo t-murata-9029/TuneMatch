d453c1c987ff01082a0b882f93bf0519462b1f71
murata <t.murata.lol.9029@gmail.com>
2025-11-21 12:33:40 +0900

    見た目変更

--- theme/theme.ts
+++ theme/theme.ts
@@ -6,11 +6,35 @@ const main_theme = createTheme({
             main: '#FF69B4',
             contrastText: '#ffffffff',
         },
+        secondary: {
+            main: '#FF7F50',
+            contrastText: '#ffffffff',
+        },
         background: {
             default: '#ffffffff',
         },
         text: { primary: '#000000' },
-    }, 
+    },
+    typography: {
+        h1: {
+            color: "#FF7F50",
+        },
+        h2: {
+            color: "#FF7F50",
+        },
+        h3: {
+            color: "#FF7F50",
+        },
+        h4: {
+            color: "#FF7F50",
+        },
+        h5: {
+            color: "#FF7F50",
+        },
+        h6: {
+            color: "#FF7F50",
+        },
+    },
     components: {
         // 既存のボタン設定
         MuiButton: {
@@ -28,6 +52,31 @@ const main_theme = createTheme({
         MuiCheckbox: {
             defaultProps: {
                 color: 'primary',
+            },
+        },
+        MuiIcon: {
+            defaultProps: {
+                color: 'secondary'
+            }
+        },
+        MuiSvgIcon: {
+            defaultProps: {
+                // デフォルトの color prop を secondary (今のテーマでは #FF7F50) に設定
+                color: 'secondary',
+            },
+        },
+        MuiCard: {
+            styleOverrides: {
+                // styleOverrides はテーマを受け取る関数形式で書くのがベストプラクティス
+                root: ({ theme }) => ({
+                    transition: theme.transitions.create(['box-shadow', 'transform' ], {
+                        duration: theme.transitions.duration.standard,
+                    }),
+                    '&:hover': {
+                        boxShadow: theme.shadows[6], 
+                        transform: 'translateY(-5px)', 
+                    },
+                }),
             },
         },
     },
