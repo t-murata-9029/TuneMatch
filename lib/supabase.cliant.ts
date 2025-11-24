ab19f42d0065283533f266f97d1379f3ae1db727
toshihide murata <t.murata.lol.9029@gmail.com>
2025-11-24 23:39:59 +0900

    createBrowserClientに変更

--- lib/supabase.cliant.ts
+++ lib/supabase.cliant.ts
@@ -1,8 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createBrowserClient } from '@supabase/ssr'
 
-export const supabase = createClient(
-  // 💡 ここに実際のURL (string) が渡されているか確認
-  process.env.NEXT_PUBLIC_SUPABASE_URL!, 
-  // 💡 ここに実際のAnon Key (string) が渡されているか確認
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+export const supabase = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
 )
