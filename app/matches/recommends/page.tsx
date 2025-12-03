1d8c156618cd3d6038e4146fe3e6d024f8ad77cd
murata <t.murata.lol.9029@gmail.com>
2025-12-03 12:27:14 +0900

    作業中

---
+++ app/matches/recommends/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { Typography } from "@mui/material";
+import { useEffect } from "react";
+
+export default function page() {
+
+    useEffect
+
+    return (
+        <Typography variant="h5">hi</Typography>
+    )
+}
