b41aa277b453dc9ad97ec17ec8d28d498a904d48
murata <t.murata.lol.9029@gmail.com>
2025-12-08 15:11:55 +0900

    少しだけUI整えたよ

--- features/routes/matches/components/RecommendsList.tsx
+++ features/routes/matches/components/RecommendsList.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react"
 import { getRecommendsList } from "../endpoints"
-import { Box, Typography } from "@mui/material";
+import { Avatar, Box, Card, CardContent, Typography } from "@mui/material";
 import { Recommends } from "../types";
 
 /**
@@ -11,7 +11,7 @@ import { Recommends } from "../types";
 export default function RecommendsList() {
     const [recommendsList, setRecommendsList] = useState<Recommends[]>();
 
-    useEffect( () => {
+    useEffect(() => {
         const setData = async () => {
             const list = await getRecommendsList();
             setRecommendsList(list)
@@ -19,12 +19,42 @@ export default function RecommendsList() {
 
         setData();
         console.log(recommendsList)
-    },[])
+    }, [])
 
-    return(
+    return (
         <>
-            <Box>
-                
+            <Box display="flex" flexDirection="column" gap={2}>
+                {recommendsList?.map((recommend) => (
+                    <Card key={recommend.user.id} sx={{ minWidth: 275 }}>
+                        <CardContent>
+                            <Box display="flex" alignItems="center" gap={2}>
+                                {/* 左：ユーザーアイコン */}
+                                <Avatar
+                                    src={"https://tpwncberbdmckktfcnpg.supabase.co/storage/v1/object/public/user_images/" + recommend.user.id + "/" + recommend.user.id}
+                                    alt={recommend.user.username}
+                                    sx={{ width: 56, height: 56 }}
+                                />
+
+                                {/* 右：情報 */}
+                                <Box>
+                                    <Typography variant="h6">
+                                        {recommend.user.username}
+                                    </Typography>
+
+                                    <Typography color="text.secondary">
+                                        類似度: {recommend.similarityScore}
+                                    </Typography>
+                                    <Typography variant="body2">
+                                        一致した項目：{recommend.matchReasons.mostSimilarKeys[0]}
+                                    </Typography>
+                                    <Typography color="text.secondary">
+                                        {recommend.user.profile_text}
+                                    </Typography>
+                                </Box>
+                            </Box>
+                        </CardContent>
+                    </Card>
+                ))}
             </Box>
         </>
     );
