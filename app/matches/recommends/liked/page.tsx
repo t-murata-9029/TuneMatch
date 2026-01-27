41f1a6d63966aba3e5e291a6ab4875a602b81cbd
murata <t.murata.lol.9029@gmail.com>
2026-01-27 11:37:27 +0900

    matchbarを追加

--- app/matches/recommends/liked/page.tsx
+++ app/matches/recommends/liked/page.tsx
@@ -1,8 +1,10 @@
 import LikedList from "@/features/routes/matches/liked/components/LikedList";
+import MatchBar from "@/features/routes/matches/recommends/components/MatchBar";
 
 export default function likedPage(){
     return(
         <>
+            <MatchBar />
             <LikedList />
         </>
     );
