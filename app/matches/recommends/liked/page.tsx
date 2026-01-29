76711277c3fbd8f9de71a6450617f05f5eb357eb
murata <t.murata.lol.9029@gmail.com>
2026-01-29 15:10:11 +0900

    幅調節

--- app/matches/recommends/liked/page.tsx
+++ app/matches/recommends/liked/page.tsx
@@ -1,11 +1,14 @@
 import LikedList from "@/features/routes/matches/liked/components/LikedList";
 import MatchBar from "@/features/routes/matches/recommends/components/MatchBar";
+import { Box, Stack } from "@mui/material";
 
-export default function likedPage(){
-    return(
+export default function likedPage() {
+    return (
         <>
-            <MatchBar />
-            <LikedList />
+            <Stack spacing={2} sx={{ p: 2 }}>
+                <MatchBar />
+                <LikedList />
+            </Stack>
         </>
     );
 }
